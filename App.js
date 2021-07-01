ef6f0f49c66bdc951914337f87d28adc27c0cd60
Hwang-yoonji <kaci00@naver.com>
2021-07-01 14:40:09 +0900

    swiper

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class SwiperComponent extends Component {
+  render() {
+    return(
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text styles={styles.text}>hi</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text styles={styles.text}>im</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text styles={styles.text}>yoonji</Text>
+        </View>
+      </Swiper>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
+
+  },
+  slide1: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  slide2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  slide3: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
+
+AppRegistry.registerComponent('swiper', () => SwiperComponent)
